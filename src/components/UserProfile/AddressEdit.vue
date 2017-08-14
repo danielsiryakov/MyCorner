26fc75c1c41f57d20d7229c0b3c2c9114d6d4633
danielsiryakov <danielsiryakov@gmail.com>
2017-08-14 12:23:36 -0400

    overhauled layout and addressbook implementation

---
+++ src/components/UserProfile/AddressEdit.vue
@@ -0,0 +1,23 @@
+<template>
+  <!-- if you want automatic padding use "layout-padding" class -->
+  <div class="layout-padding">
+    <h5 class="text-bold text-tertiary">Edit Address</h5><br>
+    {{address.line1}}<br>
+    Make this your default address?
+
+
+    <!-- your content -->
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['address'],
+    data () {
+      return {}
+    }
+  }
+</script>
+
+<style>
+</style>
--- src/components/UserProfile/Profile.vue
+++ src/components/UserProfile/Profile.vue
@@ -1,15 +1,52 @@
 <template>
-    <div class="layout-view">
+    <div class="">
       <div class ="layout-padding">
         <div class="row justify-center">
           <div class="col-lg-8">
-            <h4 class="text-bold">Your User Info!</h4>
-            <q-list highlight>
+            <h4 class="text-bold">Profile Information</h4>
+            <q-list highlight class="bg-white">
               <q-list-header>{{ user.email }}</q-list-header>
-              <q-item v-for="(item,key) in user" :key="key">
-                {{ key }} - {{ item }}
-              </q-item>
+              <q-collapsible indent label="Address Book">
+                <q-item class="group" v-for="address in this.$store.state.userInfo.address_book" :key="address.id">
+                  <span class="text-bold" style="padding-right: 10px">{{address.name}}: </span> {{address.line1}}
+                  <q-chip v-if="address.default" small color="amber-9">default</q-chip>
+                  <q-item-side right color="primary">
+                    <q-icon name="edit" @click="editAddress(address)"/>
+                  </q-item-side>
+                </q-item>
+              </q-collapsible>
+              <q-collapsible label="Track Your Order">
+                <div>
+                  Content
+                </div>
+              </q-collapsible>
+              <q-collapsible label="Past Orders/ Reorder">
+                <div>
+                  Content
+                </div>
+              </q-collapsible>
+              <q-collapsible label="Your Wallet">
+                <div>
+                  Content
+                </div>
+              </q-collapsible>
             </q-list>
+            <q-modal ref="addressDisplay" class="minimized" :content-css="{padding: '20px', maxWidth: '500px', maxHeight: '300px'}">
+              <h4 class="text-bold text-tertiary">Make this Your default address?</h4>
+              <span class="text-bold">{{ selectedAddress.line1 }}</span><br><br>
+              <div class="group">
+                <q-btn color="primary" @click="makeDefault">Make Default</q-btn>
+                <q-btn outline color="tertiary" @click="this.$refs.addressDisplay.close()">Cancel</q-btn>
+              </div>
+                <!--<address-edit :address="selectedAddress"></address-edit>-->
+            </q-modal>
+            <br>
+            <br>
+            <div class="row justify-center">
+              <div class="col-8">
+                <q-btn class="full-width" color="primary" outline @click="logout">Log out</q-btn>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -18,7 +55,12 @@
 
 <script>
   import { mapActions, mapGetters } from 'vuex'
+  import shop from '../../api/shop'
+  import AddressEdit from './AddressEdit.vue'
   export default {
+    components: {
+      AddressEdit
+    },
     computed: {
       ...mapGetters([
         'user'
@@ -26,13 +68,32 @@
     },
     methods: {
       ...mapActions([
-        'getUserInfo'
-      ])
+        'getUserInfo',
+        'logout'
+      ]),
+      editAddress (address) {
+        this.selectedAddress = address
+        this.$refs.addressDisplay.open()
+      },
+      makeDefault () {
+        shop.changeDefaultAddress(this.selectedAddress.address_id)
+      }
     },
-    mounted () {
+    created () {
       this.getUserInfo()
+    },
+    data () {
+      return {
+        selectedAddress: {}
+      }
     }
   }
 </script>
 
-<style></style>
+<style>
+  .q-item-label {
+    font-weight: bold;
+    font-size: larger;
+    color: #0f1f38;
+  }
+</style>
